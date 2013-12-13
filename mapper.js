0b422b6a83a96ad8cb4e481b2fb776a8c73d0056
Sean Walsh <seanmatthewwalsh@gmail.com>
2013-12-13 16:23:53 +0200

    Adding files

---
+++ mapper.js
@@ -0,0 +1,188 @@
+﻿// get path to this script, to link to json data files
+var ___thisPath = document.getElementsByTagName('script')[document.getElementsByTagName('script').length - 1].src.split('?')[0].split('/').slice(0, -1).join('/') + '/';
+
+$(function () {
+    "use strict";
+
+    $.widget("capesean.mapper", {
+        // default options
+        options: {
+            fillColor: "#eee",
+            fillOpacity: 0.5,
+            strokeColor: "#000",
+            strokeWidth: 1,
+            strokeOpacity: 0.7,
+            data: [],
+            dataType: "",
+            drawAll: false
+        },
+
+        // init variables
+        _minLat: null,
+        _maxLat: null,
+        _minLng: null,
+        _maxLng: null,
+
+        // load data
+        _getData: function () {
+
+            var url;
+            switch (this.options.dataType) {
+                case "provinces":
+                    url = "provinces.js";
+                    break;
+                case "districts":
+                    url = "districts.js";
+                    break;
+                case "municipalities":
+                    url = "municipalities.js";
+                    break;
+                case "wards":
+                    url = "wards.js";
+                    break;
+                default:
+                    throw ("Not Implemented dataType option in _getData: " + this.options.dataType);
+            }
+
+            var json = null;
+            $.ajax({
+                'async': false,
+                'global': false,
+                'url': ___thisPath + url,
+                'dataType': "json",
+                'success': function (data) {
+                    json = data;
+                }
+            });
+            return json;
+
+        },
+
+        // get bound limits by looping through all points
+        _getBounds: function (data) {
+
+            for (var e = 0; e < data.length; e++) { //entity
+                for (var sh = 0; sh < data[e].shapes.length; sh++) { //shape
+                    var coords = data[e].shapes[sh].coords;
+                    for (var c = 0; c < coords.length; c++) { // coord
+                        var coord = coords[c];
+                        if (this._minLat === null || coord.lat < this._minLat) this._minLat = coord.lat;
+                        if (this._maxLat === null || coord.lat > this._maxLat) this._maxLat = coord.lat;
+                        if (this._minLng === null || coord.lng < this._minLng) this._minLng = coord.lng;
+                        if (this._maxLng === null || coord.lng > this._maxLng) this._maxLng = coord.lng;
+                    }
+                }
+            }
+
+        },
+
+        // shifts elements in an array
+        _moveArrayElement: function (arr, oldIndex, newIndex) {
+
+            if (newIndex >= arr.length) newIndex = arr.length;
+            arr.splice(newIndex, 0, arr.splice(oldIndex, 1)[0]);
+
+        },
+
+        // prepares the list - filtering, ordering, etc
+        _prepareList: function (data) {
+
+            // check if no data was supplied as an option
+            if (!this.options.data || this.options.data.length == 0) return;
+
+            // for each item in the full list
+            for (var m = data.length - 1; m >= 0; m--) {
+
+                var item = data[m];
+
+                // try get it from the options list
+                var option = $.grep(this.options.data, function (e) { return e.id == item.id; })[0];
+
+                if (!option) {
+                    // wasn't supplied, so remove from the list (unless drawAll == true)
+                    if (!this.options.drawAll)
+                        data.splice(m, 1);
+                } else {
+                    // was supplied, so move to end of array (so the lines are drawn on top / last) 
+                    this._moveArrayElement(data, m, data.length);
+                    // copy the styles & other properties, eg html info window
+                    $.extend(item, option);
+                }
+            }
+        },
+
+        // the constructor
+        _create: function () {
+
+            // can only handle these data entities:
+            if (['provinces', 'districts', 'municipalities', 'wards'].indexOf(this.options.dataType) < 0) {
+                alert("Invalid dataType option");
+                return false;
+            }
+
+            // load the data
+            var data = this._getData();
+
+            // prepare the list
+            this._prepareList(data);
+
+            // find the min/max coords
+            this._getBounds(data);
+
+            // setup map
+             var mapOptions = { zoom: 0, center: new google.maps.LatLng(0, 0), mapTypeId: google.maps.MapTypeId.TERRAIN };
+            var map = new google.maps.Map(this.element[0], mapOptions),
+                bounds = new google.maps.LatLngBounds();
+
+            // setup a global infoWindow
+            var infoWindow = new google.maps.InfoWindow();
+
+            // drawing.... for each item
+            for (var m = 0; m < data.length; m++) {
+                var item = data[m];
+
+                // for each shape
+                for (var sh = 0; sh < item.shapes.length; sh++) {
+                    var coords = item.shapes[sh].coords,
+                        polygon,
+                        paths = [];
+
+                    // foreach point/coord
+                    for (var c = 0; c < coords.length; c++) {
+                        var latLng = new google.maps.LatLng(coords[c].lat, coords[c].lng);
+                        paths.push(latLng);
+                        bounds.extend(latLng);
+                    }
+
+                    // setup the polygon
+                    polygon = new google.maps.Polygon({
+                        paths: paths,
+                        strokeColor: item.strokeColor || this.options.strokeColor,
+                        strokeOpacity: item.strokeOpacity || this.options.strokeOpacity,
+                        strokeWeight: item.strokeWidth || this.options.strokeWidth,
+                        fillColor: item.fillColor || this.options.fillColor,
+                        fillOpacity: item.fillOpacity || this.options.fillOpacity,
+                        html: item.html
+                    });
+
+                    // set on map
+                    polygon.setMap(map);
+
+                    // if there's html for a popup, add the listener
+                    if (polygon.html) {
+                        google.maps.event.addListener(polygon, 'click', function(event) {
+                            infoWindow.setContent(this.html);
+                            infoWindow.setPosition(event.latLng);
+                            infoWindow.open(map);
+                        });
+                    }
+                }
+            }
+
+            // zoom to fit the bounds
+            map.fitBounds(bounds);
+        }
+    });
+});
+
+
